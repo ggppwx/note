439c088dc390c201c286acd0a65388b03414ac01
Jingwei GU <jgu45@bloomberg.net>
2016-05-03 19:01:01 -0400

    auto-commit@ 05-03-16_19:01

--- sync.sh
+++ sync.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 clear
 timestamp=$(date +"%m-%d-%y_%H:%M")
-echo "auto-commit@ $timestamp"
+echo "----------- auto-commit@ $timestamp --------------"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
-cd $DIR
-git add -u
-git commit -m "auto-commit@ $timestamp"
-git push origin master
+
+
+function git-pull
+{
+    echo "git pull ......"
+    cd $DIR
+    git pull origin master
+    exit
+}
+
+function git-autopush
+{
+    echo "git push ....."
+    cd $DIR
+    git add -u
+    git commit -m "auto-commit@ $timestamp"
+    git push origin master
+    exit
+}
+
+
+if [[ "$#" != "1" ]] ; then
+    echo "should have 1 argument !"
+    exit
+else
+    echo $1
+    if [[ $1 = "pull" ]]; then
+        git-pull
+    fi
+    if [[ $1 = "push" ]]; then
+        git-autopush
+    fi    
+    exit
+fi
+
+
+
+
+
 
