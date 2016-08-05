8d0adc0ce9c15745ce51622f548b65fc91ecbcd3
roy gu <sirgujingwei@gmail.com>
2016-08-05 02:17:49 -0400

    adding publish script

---
+++ publish.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+TIMESTAMP=`date +%Y-%m-%d`
+
+# create new file name 
+
+# convert it to html/md
+
+# copy to a cerntain directory
+
+
+# git-push to sync
+if [[ $# != "1" ]] ;then
+    echo "ERROR...."
+else
+    OLD_FILE_NAME=$1
+    TEMP_FILE_NAME="${OLD_FILE_NAME%.org}.html"
+    NEW_FILE_NAME="$TIMESTAMP-$TEMP_FILE_NAME"    
+    echo $NEW_FILE_NAME
+
+    # new file in dir ?
+    DES="/Users/jingweigu/Documents/workspace/ggppwx.github.io/_posts/"
+    MATCH=$(find $DES -name "*$TEMP_FILE_NAME")
+    echo $MATCH
+
+
+    
+    if [[ $MATCH = "" ]]; then
+	NEW_FILE_NAME="$DES$NEW_FILE_NAME"
+	echo "NOT MATCH.. run convertion: $NEW_FILE_NAME"
+    else
+	echo "MATCH.. update"
+	NEW_FILE_NAME="$MATCH"
+	echo $NEW_FILE_NAME
+    fi
+    
+    
+    
+fi
