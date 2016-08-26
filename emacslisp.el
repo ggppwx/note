8ce491d0eaf121bbe6160ae0ce609176b8a3dae0
roy gu <sirgujingwei@gmail.com>
2016-08-26 19:09:32 -0400

    auto-commit@ 08-26-16_19:09

---
+++ emacslisp.el
@@ -0,0 +1,59 @@
+;; eval-last-sexp
+(+ 1 2 )
+(< 3 4)
+
+;; print 
+(message "hi")
+
+
+(integerp 3)
+
+
+(if t "yes" "no")
+
+
+(equal 4 3)
+
+
+;; set variable
+(let (x y (z 1)) 
+(setq x 10 y 2)
+(+ x y z)
+)
+
+;; eval-region
+(progn (message "a") (message "b"))
+
+(let ((i 0))
+(while ( < i 4 )
+  (print i )
+  (setq i (+ 1 i))))
+
+
+;; eval first, then execute 
+(defun test (start, end)
+  "doc"
+  (interactive)
+  (insert "yeah")
+  )
+
+
+;; 
+(defun test1(myStart myEnd)
+  "print argument"
+  (interactive "r")
+  (message "Region begin at: %d, end at: %d" myStart myEnd)
+  )
+
+;; universal arg Ctr+u
+(defun test2(arg)
+  (interactive "p")
+  (message "arg: %d" arg)
+  )
+
+
+;; A function with the (interactive …) clause is called a command, and can be called by execute-extended-command (that is, pressing 【Alt+x】).
+
+
+
+
