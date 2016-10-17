ed51d5bfdc4f692bbee7cb40c1b9b133f678e39a
roy gu <sirgujingwei@gmail.com>
2016-10-17 18:31:59 -0400

    auto-commit@ 10-17-16_18:31

---
+++ emacsBatch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+AGENDA=$(emacs -batch -l ~/.emacs -eval '(org-batch-agenda "a")')
+echo "$AGENDA" 
+
+
+
+
+TODO=$(emacs -batch -l ~/.emacs -eval '(org-batch-agenda-csv "t")')
+# echo "$TODO" 
