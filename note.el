d6dfaeab825b762e6112c50f2664e910a75d5203
roy gu <sirgujingwei@gmail.com>
2016-12-31 17:39:50 -0500

    auto-commit@ 12-31-16_17:39

--- note.el
+++ note.el
@@ -1,10 +1,31 @@
 ;;; note.el my customized emacs function
+;;
+;;
 
 
+
+;;;###autoload
 (defun sync-push ()
+  "call sync push function, push the changes to github"
   (interactive)
   (let (cmdStr)
     (setq cmdStr "/Users/jingweigu/Documents/note/sync.sh push")
     (shell-command cmdStr)
     )
   )
+
+;;;###autoload
+(defun sync-pull ()
+  "call sync push function, push the changes to github"
+  (interactive)
+  (let (cmdStr)
+    (setq cmdStr "/Users/jingweigu/Documents/note/sync.sh pull")
+    (shell-command cmdStr)
+    )
+  )
+
+
+
+(provide 'note)
+
+;;; note.el ends here 
